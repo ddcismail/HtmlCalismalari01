502c38fe52b0b9b61e84c0b47b6e248be1e9bbe1
Ismaıl DEDEC <ismaildedec@gmail.com>
2020-10-10 20:36:21 +0300

    ismail -ihsan hesap makinesi css çalışmaları

--- css&js/script.js
+++ css&js/script.js
@@ -3,6 +3,11 @@
  alert ("HOŞ GELDİNİZ");
  };
 
+ function mesajGonder(deger) {
+    var p=document.getElementById('link1');
+     p.textContent=deger;
+    };
+
  function degistirme(){
      var p=document.getElementById('link1');
      p.textContent='Buton Ile Degisti';
